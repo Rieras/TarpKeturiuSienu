0de02b65c7e31d61f430d8e788d6ab8ebed97a11
Rieras <konra.konra@gmail.com>
2025-03-14 01:04:43 +0200

    Menus
    Added a pause button and menu, implemented a custom font for the game, added a background picture for the main menu and the options menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public bool isPaused = false;
+
+    void Start()
+    {
+  //      pauseMenu = GameObject.Find("Menu");
+        pauseMenu.SetActive(false);
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
